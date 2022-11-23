dbdf07a62e3099992d88952ea723ec6bb76f5fbe
alihaij <a3haij@gmail.com>
2022-11-24 00:28:07 +0300

    last commit

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
+
+// Setup empty JS object to act as endpoint for all routes
+
+
+// Start up an instance of app
+
+/* Dependencies */
+/* Middleware*/
+
+//Here we are configuring express to use body-parser as middle-ware.
+// Cors for cross origin allowance
+
+// Initialize the main project folder
+
+// Spin up the server
+// Callback to debug
+
+// Initialize all route with a callback function
+
+// Callback function to complete GET '/all'
+
+// Post Route
+  
+
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 const port = 8000;
-
+// Express to run server and routes
 const cors = require('cors');
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 // Require Express to run server and routes
 const app = express();
 
@@ -34,7 +58,7 @@ app.post('/postData',(req,res)=>{
     projectData ={
         temp:req.body.temp,
         date:req.body.date,
-        content:req.body.content
+        response:req.body.content
     }
     res.send(projectData).status(200).end();
     console.log(projectData);
--- website/app.js
+++ website/app.js
@@ -1,32 +1,31 @@
 /* Global Variables */
-const api = '914024967b05242786e0a0b1cc1fa08d';
-const url ='http://localhost:8000/';
+const apiKey = '914024967b05242786e0a0b1cc1fa08d';
+const url ='http://localhost:8000';
 
-const zipElement = document.getElementById('zip');
-const feelingsElement = document.getElementById('feelings');
-const dateElement = document.getElementById('date');
-const tempElemetn = document.getElementById('temp');
-const contentElement = document.getElementById('content');
+const zipElement = document.querySelector('#zip');
+const feelingsElement = document.querySelector('#feelings');
+const dateElement = document.querySelector('#date');
+const tempElemetn = document.querySelector('#temp');
+const contentElement = document.querySelector('#content');
 
 
-document.getElementById('generate').addEventListener("click" , function() {
+document.querySelector('#generate').addEventListener("click" , function() {
+    
     let data = {
         zipCode: zipElement.value,
         content:feelingsElement.value,
         date:newDate
     }
-    getInformation(data.zipCode)
-.then(zipInfo=>{
-   console.log(zipInfo);
-    data.temp= zipInfo.main.temp;
+    let getinfo = getInformation(data.zipCode)
+   console.log(getinfo);
+    data.temp= getinfo.main.temp;
     postData(data);
 });
 
-});
 
 async function getInformation(zipCode){
-    let response = await((await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${api}&units=imperial`)).json());
-//     await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${api}&units=imperial`)
+    let response = (await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}&units=imperial`)).json();
+//     await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}&units=imperial`)
 //    .then(response => console.log(response.json()))
 //    .then(json => console.log(json))
     console.log(response);
@@ -36,34 +35,34 @@ async function getInformation(zipCode){
 
 async function postData(data){
 
-    console.log(`${url}postData`);
-    let res = await fetch(`${url}postData`,{
+    console.log(`${url}/postData`);
+    let res = await fetch(`${url}/postData`,{
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body:JSON.stringify(data)});
 try{
     if(!res.ok){
-        console.log('not successfuly');
+        console.log('not successfully');
         
     }else{
         res.json().then(data=>{
-            updateUi();
-        })
+            displayData();
+        });
     }
 }catch(e){
     console.log(e);
 }
 }
 
-async function updateUi(){
+async function displayData(){
     
-    let res = await fetch(`${url}getAll`);
+    let res = await fetch(`${url}/getAll`);
     try{
         res.json().then(data =>{
             dateElement.innerHTML = `Date: ${data.date}`;
             tempElemetn.innerHTML = `Temperature: ${data.temp}`;
-            contentElement.innerHTML = `Feelings: ${data.content}`
-        })
+            contentElement.innerHTML = `Feelings: ${data.response}`;
+        });
     }catch(e){
         console.log(e);
     }
