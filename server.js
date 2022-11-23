6fafbea98535b24a3a11b1ac3a6fecfe9eb2fe49
alihaij <a3haij@gmail.com>
2022-11-23 23:24:13 +0300

    finishing the project

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
+const port = 8000;
 
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser')
 // Require Express to run server and routes
+const app = express();
+
 
 // Start up an instance of app
 
@@ -11,9 +17,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+app.listen(port,listening);
+function listening (){
+    console.log(`server Running on this Link: http://localhost:${port}`);
+}
+app.get('/getAll',(req,res)=>{
+    res.send(projectData).status(200).end();
+});
+
+app.post('/postData',(req,res)=>{
+    projectData ={
+        temp:req.body.temp,
+        date:req.body.date,
+        content:req.body.content
+    }
+    res.send(projectData).status(200).end();
+    console.log(projectData);
+});
--- website/app.js
+++ website/app.js
@@ -1,4 +1,73 @@
 /* Global Variables */
+const api = '914024967b05242786e0a0b1cc1fa08d';
+const url ='http://localhost:8000/';
+
+const zipElement = document.getElementById('zip');
+const feelingsElement = document.getElementById('feelings');
+const dateElement = document.getElementById('date');
+const tempElemetn = document.getElementById('temp');
+const contentElement = document.getElementById('content');
+
+
+document.getElementById('generate').addEventListener("click" , function() {
+    let data = {
+        zipCode: zipElement.value,
+        content:feelingsElement.value,
+        date:newDate
+    }
+    getInformation(data.zipCode)
+.then(zipInfo=>{
+   console.log(zipInfo);
+    data.temp= zipInfo.main.temp;
+    postData(data);
+});
+
+});
+
+async function getInformation(zipCode){
+    let response = await((await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${api}&units=imperial`)).json());
+//     await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${api}&units=imperial`)
+//    .then(response => console.log(response.json()))
+//    .then(json => console.log(json))
+    console.log(response);
+    return response;
+   }
+
+
+async function postData(data){
+
+    console.log(`${url}postData`);
+    let res = await fetch(`${url}postData`,{
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body:JSON.stringify(data)});
+try{
+    if(!res.ok){
+        console.log('not successfuly');
+        
+    }else{
+        res.json().then(data=>{
+            updateUi();
+        })
+    }
+}catch(e){
+    console.log(e);
+}
+}
+
+async function updateUi(){
+    
+    let res = await fetch(`${url}getAll`);
+    try{
+        res.json().then(data =>{
+            dateElement.innerHTML = `Date: ${data.date}`;
+            tempElemetn.innerHTML = `Temperature: ${data.temp}`;
+            contentElement.innerHTML = `Feelings: ${data.content}`
+        })
+    }catch(e){
+        console.log(e);
+    }
+}
 
 // Create a new date instance dynamically with JS
 let d = new Date();
