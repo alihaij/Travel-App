01a074d7be13e5d202a890f97a9c64c77c143e98
alihaij <a3haij@gmail.com>
2022-11-24 12:25:48 +0300

    add await in response

--- website/app.js
+++ website/app.js
@@ -25,7 +25,7 @@ document.querySelector('#generate').addEventListener("click", function () {
 });
 
 async function getInformation(zipCode) {
-    let response = (await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}&units=imperial`)).json();
+    let response = await(await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}&units=imperial`)).json();
     //     await fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${apiKey}&units=imperial`)
     //    .then(response => console.log(response.json()))
     //    .then(json => console.log(json))
