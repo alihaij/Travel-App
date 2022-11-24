343591a8927c486c8e1f928cb235092a9504f584
alihaij <a3haij@gmail.com>
2022-11-24 17:27:16 +0300

    +1

--- website/app.js
+++ website/app.js
@@ -72,4 +72,4 @@ async function displayData() {
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+let newDate = d.getMonth() +1+ '.' + d.getDate() + '.' + d.getFullYear();
